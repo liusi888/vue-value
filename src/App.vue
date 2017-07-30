b07a33b73a683e0566e19a4f086dcbeccced36b7
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2017-07-30 22:54:52 +0800

    vue by value

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <components-a
+      msgFromFather="I am a father"
+      v-on:child-tell-me-something="lisenToMyBoy"
+     ></components-a>
+     <p>child tell me {{childWords}}</p>
+  </div>
+</template>
+
+<script>
+import ComponentsA from './components/componentsA'
+
+export default {
+  //1.注册 父-子
+  components: {ComponentsA},
+  data() {
+    return{
+      childWords:''
+    }
+  },
+  methods: {
+    //2.绑定子组件事件 子-父
+    lisenToMyBoy(msg){
+     // console.log(msg);
+     this.childWords=msg
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/componentsA.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+      <h1>{{msg}}</h1>
+      <button v-on:click="onClickMe">Click!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  //1.接收 父-子
+  props:['msgFromFather'],
+  data() {
+    return {
+      msg:'hello from component A'
+    }
+  },
+  methods: {
+    onClickMe() {
+         //1.打印
+        //console.log(this.msgFromFather);
+        //2.发送给父级
+        this.$emit('child-tell-me-something',this.msg);
+     }
+  }
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
